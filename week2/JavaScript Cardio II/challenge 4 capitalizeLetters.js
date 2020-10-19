86fe8436d1d92b34d75fff832ea7c0b9e6467f97
Dima <dimakaddah@gmail.com>
2020-10-19 23:46:30 +0200

    add challenge 4

--- week2/JavaScript Cardio II/challenge 4 capitalizeLetters.js
+++ week2/JavaScript Cardio II/challenge 4 capitalizeLetters.js
@@ -2,6 +2,23 @@
 // Return a string with the first letter of every word capitalized
 // ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
 function capitalizeLetters(str) {
+  //1 SOLUTION
+  // const strArr = str.toLowerCase().split(' ');
+  // for (let i = 0; i < strArr.length; i++) {
+  //   strArr[i] = strArr[i].substring(0, 1).toUpperCase() + strArr[i].substring(1);
+  // }
+  // return strArr.join(' ');
+  ///////////////////////////////
+
+  //2 SOLUTION
+  // return str
+  //   .toLowerCase()
+  //   .split(' ')
+  //   .map(word => word[0].toUpperCase() + word.substr(1)).join(' ');
+  ///////////////////////////////
+
+  //3 SOLUTION
+  // return str.replace(/\b[a-z]/gi, (char) => char.toUpperCase());
 
 }
 const output = capitalizeLetters('i love javascript');
