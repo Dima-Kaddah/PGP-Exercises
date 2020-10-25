3abc2252209b399cdb5dcd58d3438690321940fe
Dima <dimakaddah@gmail.com>
2020-10-25 18:48:20 +0100

    add exercise 4

--- week3/Algorithms III/BasicAlgorithms.js
+++ week3/Algorithms III/BasicAlgorithms.js
@@ -47,3 +47,24 @@ function getIndexToIns(arr, num) {
 }
 
 console.log(getIndexToIns([40, 60], 50));
+
+// Basic Algorithm Scripting: Mutations
+// Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+// For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+// The arguments ["hello", "hey"] should return false because the string "hello" does not contain a "y".
+// Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".
+//4
+function mutation(arr) {
+  let first = arr[0].toLowerCase();
+  let second = arr[1].toLowerCase();
+  for (let i = 0; i < second.length; i++) {
+    let letter = second[i];
+    if (first.indexOf(letter) === -1) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(mutation(["hello", "hey"]));
